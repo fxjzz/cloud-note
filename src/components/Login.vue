b73b2c6559fbc8ceff9a5500be92c3fc77ab46e5
lzxzz <1042183935@qq.com>
2022-10-25 19:50:34 +0800

    init

---
+++ src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="login">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Login',
+  data(){
+    return{
+      msg:'this is a login page'
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
